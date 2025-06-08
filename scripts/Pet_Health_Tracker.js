ea55c2f91cfa962b08c27d0c0de07463604add0d
smdsniffersd <164409560+smdsniffersd@users.noreply.github.com>
2025-06-08 20:23:16 +0300

    Delete scripts directory

--- scripts/Pet_Health_Tracker.js
+++
@@ -1,7 +0,0 @@
-  function toggleAccordion(button) {
-    const dayBlock = button.parentElement;
-    const content = dayBlock.querySelector('.accordion-content');
-    
-    button.classList.toggle('active');
-    content.classList.toggle('active');
-  }
